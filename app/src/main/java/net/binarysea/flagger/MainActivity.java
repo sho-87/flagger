4d30a742dd700b2a34ce666285dd1e20b6092e8f
Simon Ho <simonsays87@googlemail.com>
2020-02-12 11:12:04 -0800

    Initial file writing

--- app/src/main/java/net/binarysea/flagger/MainActivity.java
+++ app/src/main/java/net/binarysea/flagger/MainActivity.java
@@ -3,6 +3,7 @@ package net.binarysea.flagger;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,12 +11,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
+    FileOutputStream f;
+    PrintWriter pw;
     LayoutInflater inflater;
     View mView;
     AlertDialog.Builder builder;
@@ -60,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
                         .setPositiveButton("Create", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
                                 idValue.setText(idInput.getText().toString());
+                                createNewFile(Integer.toString(id));
                             }
                         })
                         .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -89,5 +101,40 @@ public class MainActivity extends AppCompatActivity {
         Button button4 = findViewById(R.id.button4);
         Button button5 = findViewById(R.id.button5);
         Button button6 = findViewById(R.id.button6);
+    }
+
+    private void createNewFile(String id){
+        String pathToExternalStorage = Environment.getExternalStorageDirectory().toString();
+        File exportDir = new File(pathToExternalStorage, "/Flagger");
+
+        if (!exportDir.exists()) {
+            boolean created = exportDir.mkdirs();
+        }
+
+        final DateTimeFormatter formatter =
+                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        final long unixTime = System.currentTimeMillis() - timeOffset;
+        final String formattedDtm = Instant.ofEpochSecond(unixTime)
+                .atZone(ZoneId.of("GMT-8"))
+                .format(formatter);
+
+        File file = new File(exportDir, String.format("%s - %s.csv", id, formattedDtm));
+
+        try {
+            f = new FileOutputStream(file);
+            pw = new PrintWriter(f);
+            pw.println("id, time, code");
+            pw.println("id, time, code");
+            pw.flush();
+            pw.close();
+            f.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Log.i(TAG, "******* File not found. Did you" +
+                    " add a WRITE_EXTERNAL_STORAGE permission to the manifest?");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
